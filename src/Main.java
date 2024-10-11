335b39e0e236b154bb5a3e1395a84e3811274727
sam rose <samanthaonwukaugc@gmail.com>
2024-10-11 13:14:20 -0400

    made some final changes to the file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer:");
+        int myInt = scanner.nextInt();
+
+        System.out.print("Enter a double");
+        double myDouble = scanner.nextDouble();
+
+        System.out.printf("you entered the integer: %d and the double: %.2f",myInt,myDouble);
     }
 }
